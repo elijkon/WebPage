ee447d8fc99e79922e33c2c9c47180b2fbe4f17b
Elijah Konkle <169085768+elijkon@users.noreply.github.com>
2025-04-14 12:36:20 -0500

    Update script.js

--- script.js
+++ script.js
@@ -6,8 +6,12 @@ document.addEventListener("DOMContentLoaded", function () {
             data.forEach(item => { // Loop through each item in the JSON file
                 let div = document.createElement("div"); // Create a new div for each item
                 div.innerHTML = `
-                    <h2>${item.title}</h2>
-                    <p>${item.description}</p>
+                    <h2>${item.Model}</h2>
+                    <p>${item.Manufacturer}</p>
+                    <p>${item.Engine_Type}</p>
+                    <p>${item.sixty}</p>
+                    <p>${item.Color}</p>
+
                     <a href="item-template.html?id=${item.id}">View More</a>
                 `; // Insert item data and link
                 container.appendChild(div); // Add item to the page
